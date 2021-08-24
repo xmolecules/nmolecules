e83c25f1730a77d7370006e0173ec1f9cda726be
Henning Schwentner <hs@wps.de>
2021-08-24 15:29:19 +0200

    Introduce [Identity] type attribute

---
+++ src/nMolecules.DDD/Attributes/Identity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace NMolecules.DDD
+{
+    /// <summary>
+    /// Declares a field of a class to constitute the identity of the corresponding class. Primarily used in
+    /// <code>AggregateRoot</code> and <code>Entity</code> types.
+    ///
+    /// <see href="https://domainlanguage.com/wp-content/uploads/2016/05/DDD_Reference_2015-03.pdf">Domain-Driven Design
+    ///      Reference (Evans) - Entities</see>
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field)]
+    public class IdentityAttribute : Attribute
+    {
+    }
+}
